10639d4c12dd898cb3f70afbd58733e15f06b3ac
Alexander Ruban <alexruban29@gmail.com>
2025-05-22 14:47:52 -0700

    Finished livecode in class.

--- src/PQPractice.java
+++ src/PQPractice.java
@@ -1,5 +1,77 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+
 public class PQPractice {
+
+    private record GraphicMemoir(String name, int interest) implements Comparable<GraphicMemoir> {
+
+        @Override
+        public int compareTo(GraphicMemoir other) {
+            return Integer.compare(this.interest, other.interest);
+        }
+    }
+
     public static void main(String[] args) {
-        
+
+        PriorityQueue<GraphicMemoir> pq = new PriorityQueue<>(Comparator.reverseOrder());
+
+        pq.add(new GraphicMemoir("I'm a Wild Seed", 62));
+        pq.add(new GraphicMemoir("The Third Person", 83));
+        pq.add(new GraphicMemoir("Calling Dr. Laura", 55));
+        pq.add(new GraphicMemoir("The Bride Was a Boy", 100));
+
+        System.out.println(pq.poll());
+        System.out.println(pq.poll());
+
+        // PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        // pq.add(7);
+        // pq.add(33);
+        // pq.add(2);
+        // pq.add(99);
+        // pq.poll();
+        // pq.poll();
+
+        // System.out.println();
+        // System.out.println();
+        // System.out.println(pq.peek());
+        // System.out.println(pq.poll());
+
+        List<Integer> nums = List.of(33, 2, 6, 29, 343, 966, 76, 45, 36);
+        System.out.println(topK(nums, 4));
+        System.out.println(topKEfficient(nums, 4));
+    }
+
+    // Return the top K elements in the list
+    // The original list is NOT modified
+    public static List<Integer> topK(List<Integer> nums, int k) {
+        List<Integer> copy = new ArrayList<>(nums);
+        Collections.sort(copy);
+        return copy.subList(nums.size() - k, nums.size());
+    }
+
+    public static List<Integer> topKEfficient(List<Integer> nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int num : nums) {
+            if (pq.size() < k) {
+                pq.add(num);
+            } else if (num > pq.peek()) {
+                pq.poll();
+                pq.add(num);
+            }
+        }
+
+        // List<Integer> result = new ArrayList<>();
+        // while(!pq.isEmpty()) {
+        //     result.add(pq.poll());
+        // }
+
+        List<Integer> result = new ArrayList<>(pq);
+        Collections.sort(result);
+        return result;
     }
 }
